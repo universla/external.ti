a70e9a8a8ddfa89d73190600e10ba202a2aab5d6
universla <ramirezrodriguezirma292@gmail.com>
2025-09-13 04:39:58 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import BotProfile from './pages/BotProfile';
+import AddBot from './pages/AddBot';
+import Leaderboard from './pages/Leaderboard';
+import './styles/main.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/bot/:id" element={<BotProfile />} />
+            <Route path="/add" element={<AddBot />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
